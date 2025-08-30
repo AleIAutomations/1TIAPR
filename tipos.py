d7942f94b9e73ddd0c0ef2f69dc9bd1bfef8d1d9
AleIAutomations <Ale87041729@gmail.com>
2025-08-29 23:00:42 -0300

    TIPOS

---
+++ tipos.py
@@ -0,0 +1,29 @@
+var = 1
+var2 = 10.4
+var = 15.2
+
+#comentar o meu codigo
+nome = 'Alexandre' + '10'
+
+booleano = True
+booleano = False
+
+
+lista = ['macarão', 'pão', 'arroz', 'molho'] 
+lista = [1,2,3,4]
+
+'''
+NÃO É UM CODIGO
+ESSA PARTE
+É APENAS UM AVISO
+QUE PODER SER QUALQUER UM
+E QUE NÃO VAI APARECER
+ASSIM QUE COMENTA EM CODIGOS
+
+'''
+
+
+resultado = var + var2
+print(resultado)
+
+print(nome)
