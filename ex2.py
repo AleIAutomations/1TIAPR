5bae20a04bb204c2057a76b38dff3c5d0a140228
AleIAutomations <Ale87041729@gmail.com>
2025-09-02 22:19:26 -0300

    exercicios de aula

---
+++ ex2.py
@@ -0,0 +1,15 @@
+nome = 'Alexandre'
+cpf = 859050
+sexo = True #true masculino
+nota_dessa_materia = 9.5
+
+print ('nome', nome)
+print ('cpf', cpf)
+print ('sexo', sexo)
+
+print ('nome', nome, cpf, sexo, nota_dessa_materia)
+
+
+
+
+
---
+++ ex3.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+c = 0
+
+print ('var A:', a)
+print ('Var B:', b)
