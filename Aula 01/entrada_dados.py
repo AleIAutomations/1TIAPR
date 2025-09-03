ab596c3607c207943f76b7123f924326b30222a2
AleIAutomations <Ale87041729@gmail.com>
2025-09-02 22:35:27 -0300

    exercicio 4

---
+++ Aula 01/entrada_dados.py
@@ -0,0 +1,4 @@
+nome = input ('qual o seu nome? ')
+
+print('ola, ' + nome + '! Seja bem-vindo(a)! ')
+print(f'olá, {nome}! Seja bem-vindo')
---
+++ Aula 01/ex4.py
@@ -0,0 +1,8 @@
+pi = 3.14159
+r = 2
+raio = 5
+
+a = pi * raio ** r
+print ('a formula da area e igual á:', a)
+
+
